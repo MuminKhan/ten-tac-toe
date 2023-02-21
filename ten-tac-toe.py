50f071df89dfc9f0cfdb3bc64f73bedc294c4f58
Mumin Khan <mumin@berkeley.edu>
2023-02-21 11:06:18 -0800

    adds more game logic

--- ten-tac-toe.py
+++ ten-tac-toe.py
@@ -15,13 +15,18 @@ class GameSymbol(Enum):
     O = auto()
 
 
+class SumGridMappings(Enum):
+    ROW  = 0
+    COL  = 1
+    DIAG = 2
+
 class GameGrid():
 
     def __init__(self, grid_dimensions=3) -> None:
 
         if grid_dimensions % 2 != 1:
             raise Exception('Grid size must be odd.')
-
+        
         self.grid_size = grid_dimensions
         self.grid: np.array = self.__initialize_grid(grid_dimensions)
         self.sums_grid: np.array = self.__initialize_grid(grid_dimensions)
@@ -69,10 +74,76 @@ class CPU():
     def __init__(self, grid) -> None:
         self.__grid: GameGrid = grid
 
+
+    def __choose_cell(self, axis, pos) -> Tuple:
+        grid = self.__grid
+
+        row = None
+        col = None
+
+        # axis can be 0 -> row, 1 -> col, 2 -> diagonal
+        # [row_0, row_1,  row_2]
+        # [col_0, col_1,  col_2]
+        # [diag, antidiag, 0]
+        if axis == SumGridMappings.ROW.value:
+            row = pos
+            for i in range(grid.grid_size):
+                if grid.grid[row][i] == GameEntity.NOBODY.value:
+                    col = i
+                    break
+
+        if axis == SumGridMappings.COL.value:
+            col = pos
+            for i in range(grid.grid_size):
+                if grid.grid[i][col] == GameEntity.NOBODY.value:
+                    row = i
+                    break
+
+        if axis == SumGridMappings.DIAG.value:
+            for i in range(grid.grid_size):
+                if grid.grid[i][i] == GameEntity.NOBODY.value:
+                    row = i
+                    col = i
+                    break
+
+        return (row, col)
+
+
     def get_move(self):
-        mid = self.__grid.grid_size // 2
-        if self.__grid[mid][mid] == GameEntity.NOBODY.value:
-            return (mid, mid)
+        grid = self.__grid
+        move = None
+
+        # Win, if CPU can
+        CPU_winning_score = grid.grid_size * GameEntity.CPU.value
+        moves = np.where(grid.sums_grid == CPU_winning_score - GameEntity.CPU.value)
+        candidate_moves = [(axis, pos) for axis, pos in zip(moves[0], moves[1])]
+        if len(candidate_moves) > 0:
+            row, col = candidate_moves[0]
+            #move = self.__choose_cell(row, col)
+            
+
+        # Block User from winning
+        user_winning_score = grid.grid_size * GameEntity.USER.value
+        moves = np.where(grid.sums_grid == user_winning_score - GameEntity.USER.value)
+        candidate_moves = [(axis, pos) for axis, pos in zip(moves[0], moves[1])]
+        if len(candidate_moves) > 0:
+            row, col = candidate_moves[0]
+            move = self.__choose_cell(row, col)
+
+        # Take middle spot if open
+        mid = grid.grid_size // 2
+        if grid.grid[mid][mid] == GameEntity.NOBODY.value:
+            move = (mid, mid)
+
+        # Maximize CPU score
+        CPU_winning_score = grid.grid_size * GameEntity.CPU.value
+        moves = np.where(grid.sums_grid == CPU_winning_score - GameEntity.CPU.value)
+        candidate_moves = [(axis, pos) for axis, pos in zip(moves[0], moves[1])]
+        if len(candidate_moves) > 0:
+            row, col = candidate_moves[0]
+            move = self.__choose_cell(row, col)
+        
+        return move
 
 
 class GameManager():
@@ -86,13 +157,18 @@ class GameManager():
 
     def clear_display(self):
         print("\033[H\033[J", end="")
-
+        
     def display_header(self):
         print('***** TEN! TAC! TOE! *****\n')
 
     def display_grid(self):
-        print(self.__grid)
-
+        rows = []
+        for i in range(self.__grid.grid_size-1, -1, -1):
+            cells = [n for n in range((3*i)+1, (3*i)+4)]
+            rows.append('  {} | {} | {}  \n'.format(*cells))
+        horizontal_sep = '----+---+----\n'
+        print(horizontal_sep.join(rows))
+    
     def __get_user_input(self, prompt: str, expected_type=str):
         user_input = None
         while not user_input:
