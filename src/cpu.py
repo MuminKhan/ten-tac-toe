25f944a0f22dc15dae710494b985a0e5376ea22c
Mumin Khan <mumin.m.khan@gmail.com>
2023-02-22 09:31:34 -0800

    cleaned up CPU logic

--- src/cpu.py
+++ src/cpu.py
@@ -11,74 +11,49 @@ class CPU():
     def __init__(self, grid) -> None:
         self.__grid: GameGrid = grid
 
-
     def __choose_cell(self, axis, pos) -> Tuple:
         grid = self.__grid
+        open_slots = np.ma.masked_array(grid.grid == 0)
 
         row = None
         col = None
-
-        # axis can be 0 -> row, 1 -> col, 2 -> diagonal
-        # [row_0, row_1,  row_2]
-        # [col_0, col_1,  col_2]
-        # [diag, antidiag, 0]
         if axis == SumGridMappings.ROW.value:
             row = pos
-            for i in range(grid.grid_size):
-                if grid.grid[row][i] == GameEntity.NOBODY.value:
-                    col = i
-                    break
-
-        if axis == SumGridMappings.COL.value:
+            col = np.where(open_slots[row] == True)
+            col = col[0][0]
+        elif axis == SumGridMappings.COL.value:
+            row, _ = np.where(open_slots[:axis] == True)
+            row = row[0]
             col = pos
-            for i in range(grid.grid_size):
-                if grid.grid[i][col] == GameEntity.NOBODY.value:
-                    row = i
-                    break
-
-        if axis == SumGridMappings.DIAG.value:
-            for i in range(grid.grid_size):
-                if grid.grid[i][i] == GameEntity.NOBODY.value:
-                    row = i
-                    col = i
-                    break
+        elif axis == SumGridMappings.DIAG.value:
+            diag_mask = np.zeros_like(grid.grid, dtype=bool)
+            np.fill_diagonal(diag_mask, val=True)
+            if pos:
+                diag_mask = np.fliplr(diag_mask)
+            row, col = np.argwhere(diag_mask * open_slots)[0]
 
         return (row, col)
 
-
-    def get_move(self):
+    def get_move(self) -> Tuple[int, int]:
         grid = self.__grid
-        move = None
 
-        # Win, if CPU can
-        CPU_winning_score = grid.grid_size * GameEntity.CPU.value
-        moves = np.where(grid.sums_grid == CPU_winning_score - GameEntity.CPU.value)
-        candidate_moves = [(axis, pos) for axis, pos in zip(moves[0], moves[1])]
-        if len(candidate_moves) > 0:
-            row, col = candidate_moves[0]
-            #move = self.__choose_cell(row, col)
-            
-
-        # Block User from winning
         user_winning_score = grid.grid_size * GameEntity.USER.value
-        moves = np.where(grid.sums_grid == user_winning_score - GameEntity.USER.value)
-        candidate_moves = [(axis, pos) for axis, pos in zip(moves[0], moves[1])]
-        if len(candidate_moves) > 0:
-            row, col = candidate_moves[0]
-            move = self.__choose_cell(row, col)
+        CPU_winning_score = grid.grid_size * GameEntity.CPU.value
 
-        # Take middle spot if open
+        # CPU Wins
+        targets = list(np.argwhere(grid.sums_grid == CPU_winning_score - GameEntity.CPU.value))
+        # Block User Win
+        targets += list(np.argwhere(grid.sums_grid == user_winning_score - GameEntity.USER.value))
+        if len(targets) > 0:
+            return self.__choose_cell(*targets[0])
+
+        # Take middle spot
         mid = grid.grid_size // 2
         if grid.grid[mid][mid] == GameEntity.NOBODY.value:
-            move = (mid, mid)
+            return (mid, mid)
 
-        # Maximize CPU score
-        CPU_winning_score = grid.grid_size * GameEntity.CPU.value
-        moves = np.where(grid.sums_grid == CPU_winning_score - GameEntity.CPU.value)
-        candidate_moves = [(axis, pos) for axis, pos in zip(moves[0], moves[1])]
-        if len(candidate_moves) > 0:
-            row, col = candidate_moves[0]
-            move = self.__choose_cell(row, col)
-        
-        return move
+        # Greedy on row / cols
+        NotImplementedError('Strategy not implemented yet...')
 
+        # Take any available corner spot
+        NotImplementedError('Strategy not implemented yet...')
